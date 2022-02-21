013cc27e2592894df112744470c7ace3666336a3
johnwslee <john.ws.lee@gmail.com>
2022-02-21 10:47:40 -0800

    improves the codes by assigning default values

--- src/extrucal/extrucal.py
+++ src/extrucal/extrucal.py
@@ -2,25 +2,30 @@ import pandas as pd
 import numpy as np
 import altair as alt
 
-def throughput(size, pitch, w_flight, n_flight, depth, rpm, density):
+
+def throughput(size, depth, density, rpm=1, pitch=None, w_flight=None, n_flight=1):
     """
     Calculates the extrusion throughput (Drag Flow) given the screw size, RPM,
     the channel depth of metering channel, and screw pitch
     ----------
     size     : float
-               screw size [mm]
-    pitch    : float
-               screw pitch [mm]
-    w_flight : float
-               flight width [mm]
-    n_flight : int
-               number of flight [ea]
+               Screw size [mm]
     depth    : float
-               the channel depth of metering section [mm]
-    rpm      : float
-               screw RPM
+               Channel depth of metering section [mm]
     density  : float
-               melt density of polymeric material [kg/m^3]
+               Melt density of polymeric material [kg/m^3]
+    rpm      : float
+               Screw RPM
+               Default value is 1 (throughput per unit rpm)
+    pitch    : float
+               Screw pitch [mm]
+               If None, squared pitch (=1D) is used
+    w_flight : float
+               Flight width [mm]
+               If None, 10% of screw size is used for flight width
+    n_flight : int
+               Number of flight [ea]
+               Default value is 1 (single-flighted)
 
     Returns
     -------
@@ -28,8 +33,13 @@ def throughput(size, pitch, w_flight, n_flight, depth, rpm, density):
 
     Examples
     --------
-    >>> output_per_rpm(size=100, pitch=10, depth=5)
+    >>> throughput(size=200, depth=10, density=800)
     """
+    # Assign default values
+    if pitch == None:
+        pitch = size
+    if w_flight == None:
+        w_flight = size*0.1
     
     # Calculate basic variables
     screw_root_size = size - (depth*2)
@@ -59,33 +69,45 @@ def throughput(size, pitch, w_flight, n_flight, depth, rpm, density):
     return round(throughput_per_hr, 2)
 
 
-def throughput_table(size, pitch, w_flight, n_flight, min_depth, max_depth, delta_depth, min_rpm, max_rpm, delta_rpm, density):
+def throughput_table(
+    size, density, pitch=None, w_flight=None, n_flight=1, min_depth=None, 
+    max_depth=None, delta_depth=None, min_rpm=5, max_rpm=50, delta_rpm=5
+):
     """
     Generate a table containing the volumetric throughput with respect to 
     channel depth and screw RPM
     ----------
     size        : float
                   screw size [mm]
-    pitch       : flot
-                  screw pitch [mm]
-    w_flight    : float
-                  flight width [mm]
-    n_flight    : int
-                  number of flight [ea]
-    min_depth   : float
-                  minimum depth for calculation [mm]
-    max_depth   : float
-                  maximum depth for calculation [mm]
-    delta_depth : float
-                  the amount of increment in depth for calculation [mm]
-    min_rpm     : float
-                  minimum screw RPM for calculation [RPM]
-    max_rpm     : float
-                  maximum screw RPM for calculation [RPM]
-    delta_rpm   : float
-                  the amount of increment in RPM for calculation [RPM]
     density     : float
                   melt density of polymeric material [kg/m^3]
+    pitch       : float
+                  Screw pitch [mm]
+                  If None, squared pitch (=1D) is used
+    w_flight    : float
+                  Flight width [mm]
+                  If None, 10% of screw size is used for flight width
+    n_flight    : int
+                  Number of flight [ea]
+                  Default value is 1 (single-flighted)
+    min_depth   : float
+                  minimum depth for calculation [mm]
+                  If None, 2% of screw size is used for minimum depth
+    max_depth   : float
+                  maximum depth for calculation [mm]
+                  If None, 9% of screw size is used for maximum depth
+    delta_depth : float
+                  the amount of increment in depth for calculation [mm]
+                  If None, 1% of screw size is used for depth increment
+    min_rpm     : float
+                  minimum screw RPM for calculation [RPM]
+                  Default value is 5 (5RPM)
+    max_rpm     : float
+                  maximum screw RPM for calculation [RPM]
+                  Default value is 50 (50RPM)
+    delta_rpm   : float
+                  the amount of increment in RPM for calculation [RPM]
+                  Default value is 5 (5RPM)
 
     Returns
     -------
@@ -94,50 +116,77 @@ def throughput_table(size, pitch, w_flight, n_flight, min_depth, max_depth, delt
 
     Examples
     --------
-    >>> output_table(size=100, pitch=10, min_depth=3, max_depth=7, delta_depth=1, min_rpm=10, max_rpm=50, delta_rpm=10)
+    >>> output_table(size=200, density=800)
     """
+    # Assign default values
+    if pitch == None:
+        pitch = size
+    if w_flight == None:
+        w_flight = size*0.1
+    if min_depth == None:
+        min_depth = size * 0.02
+    if max_depth == None:
+        max_depth = size * 0.09
+    if delta_depth == None:
+        delta_depth = size * 0.01
+    
+    # Generates table
     table = dict()
     throughput_list = list()
-    depth = [i for i in np.arange(min_depth, max_depth+0.1, delta_depth)]
+    depth = [round(i, 2) for i in np.arange(min_depth, max_depth+0.1, delta_depth)]
     rpm = [j for j in np.arange(min_rpm, max_rpm+0.1, delta_rpm)]
     rpm_title = [f"rpm={k}" for k in np.arange(min_rpm, max_rpm+0.1, delta_rpm)]
     for d in depth:
         for r in rpm:
-            throughput_list.append(throughput(size, pitch, w_flight, n_flight, d, r, density))
+            throughput_list.append(throughput(size, d, density, r, pitch, w_flight, n_flight))
         table[f"depth={d}"] = throughput_list
         throughput_list = []
     table_df = pd.DataFrame(table, index=rpm_title)
-    print(f"\n\033[1mThroughput[kg/hr] at {min_rpm}~{max_rpm}RPM for channel depths from {min_depth} to {max_depth}mm\033[0m\n")
+    print(
+        f"\n\033[1mThroughput[kg/hr] at {min_rpm}~{max_rpm}RPM for channel depths from {min_depth} to {max_depth}mm\033[0m\n"
+    )
     return table_df
 
 
-def throughput_plot(size, pitch, w_flight, n_flight, min_depth, max_depth, delta_depth, min_rpm, max_rpm, delta_rpm, density):
+def throughput_plot(
+    size, density, pitch=None, w_flight=None, n_flight=1, min_depth=None, 
+    max_depth=None, delta_depth=None, min_rpm=0, max_rpm=50, delta_rpm=1
+):
     """
     Generates a plot containing the volumetric throughput with respect to 
     channel depth and screw RPM
     ----------
     size        : float
                   screw size [mm]
-    pitch       : flot
-                  screw pitch [mm]
-    w_flight    : float
-                  flight width [mm]
-    n_flight    : int
-                  number of flight [ea]
-    min_depth   : float
-                  minimum depth for calculation [mm]
-    max_depth   : float
-                  maximum depth for calculation [mm]
-    delta_depth : float
-                  the amount of increment in depth for calculation [mm]
-    min_rpm     : float
-                  minimum screw RPM for calculation [RPM]
-    max_rpm     : float
-                  maximum screw RPM for calculation [RPM]
-    delta_rpm   : float
-                  the amount of increment in RPM for calculation [RPM]
     density     : float
                   melt density of polymeric material [kg/m^3]
+    pitch       : float
+                  Screw pitch [mm]
+                  If None, squared pitch (=1D) is used
+    w_flight    : float
+                  Flight width [mm]
+                  If None, 10% of screw size is used for flight width
+    n_flight    : int
+                  Number of flight [ea]
+                  Default value is 1 (single-flighted)
+    min_depth   : float
+                  minimum depth for calculation [mm]
+                  If None, 2% of screw size is used for minimum depth
+    max_depth   : float
+                  maximum depth for calculation [mm]
+                  If None, 9% of screw size is used for maximum depth
+    delta_depth : float
+                  the amount of increment in depth for calculation [mm]
+                  If None, 1% of screw size is used for depth increment
+    min_rpm     : float
+                  minimum screw RPM for calculation [RPM]
+                  Default value is 0 (0RPM)
+    max_rpm     : float
+                  maximum screw RPM for calculation [RPM]
+                  Default value is 50 (50RPM)
+    delta_rpm   : float
+                  the amount of increment in RPM for calculation [RPM]
+                  Default value is 1 (1RPM)
 
     Returns
     -------
@@ -146,15 +195,28 @@ def throughput_plot(size, pitch, w_flight, n_flight, min_depth, max_depth, delta
 
     Examples
     --------
-    >>> output_plot(size=100, pitch=10, min_depth=3, max_depth=7, delta_depth=1, min_rpm=10, max_rpm=50, delta_rpm=10)
+    >>> output_plot(size=200, density=800)
     """
+    # Assign default values
+    if pitch == None:
+        pitch = size
+    if w_flight == None:
+        w_flight = size*0.1
+    if min_depth == None:
+        min_depth = size * 0.02
+    if max_depth == None:
+        max_depth = size * 0.09
+    if delta_depth == None:
+        delta_depth = size * 0.01
+    
+    # Generate table for plot
     table = dict()
     throughput_list = list()
-    depth = [i for i in np.arange(min_depth, max_depth+0.1, delta_depth)]
+    depth = [round(i, 2) for i in np.arange(min_depth, max_depth+0.1, delta_depth)]
     rpm = [j for j in np.arange(min_rpm, max_rpm+0.1, delta_rpm)]
     for d in depth:
         for r in rpm:
-            throughput_list.append(throughput(size, pitch, w_flight, n_flight, d, r, density))
+            throughput_list.append(throughput(size, d, density, r, pitch, w_flight, n_flight))
         table[d] = throughput_list
         throughput_list = []
     table_df = pd.DataFrame(table, index=rpm)
@@ -162,13 +224,16 @@ def throughput_plot(size, pitch, w_flight, n_flight, min_depth, max_depth, delta
     table_for_plot = table_for_plot.rename(columns={"index": "RPM"})
     table_for_plot = table_for_plot.melt(id_vars="RPM", var_name="depth", value_name="throughput")
     table_for_plot["depth"] = table_for_plot["depth"].astype('category')
-    plot = alt.Chart(table_for_plot, title='Throughput vs Screw RPM & Channel Depth').mark_line().encode(
+    
+    # Generate plot
+    plot = alt.Chart(table_for_plot, title='Throughput vs Screw RPM & Channel Depth').mark_circle().encode(
         alt.X("RPM", title="Screw RPM", scale=alt.Scale(domain=(0, max_rpm))),
         alt.Y("throughput", title="Throughput [kg/hr]"),
         alt.Color(
             "depth", title="Channel depth [mm]", 
             sort=alt.EncodingSortField('throughput', op='mean', order='descending')
-        )
+        ),
+        tooltip=["RPM", "depth", "throughput"]
     ).configure_axis(
         labelFontSize=14, titleFontSize=16
     ).configure_legend(
