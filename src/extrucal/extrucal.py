c1d0d091525efafed8e1010046d9f71d4a5c2f8a
johnwslee <john.ws.lee@gmail.com>
2022-02-21 11:46:53 -0800

    Adds tests to the functions

--- src/extrucal/extrucal.py
+++ src/extrucal/extrucal.py
@@ -8,19 +8,19 @@ def throughput(size, depth, density, rpm=1, pitch=None, w_flight=None, n_flight=
     Calculates the extrusion throughput (Drag Flow) given the screw size, RPM,
     the channel depth of metering channel, and screw pitch
     ----------
-    size     : float
+    size     : int or float
                Screw size [mm]
-    depth    : float
+    depth    : int or float
                Channel depth of metering section [mm]
-    density  : float
+    density  : int or float
                Melt density of polymeric material [kg/m^3]
-    rpm      : float
+    rpm      : int or float
                Screw RPM
                Default value is 1 (throughput per unit rpm)
-    pitch    : float
+    pitch    : int or float
                Screw pitch [mm]
                If None, squared pitch (=1D) is used
-    w_flight : float
+    w_flight : int or float
                Flight width [mm]
                If None, 10% of screw size is used for flight width
     n_flight : int
@@ -35,12 +35,61 @@ def throughput(size, depth, density, rpm=1, pitch=None, w_flight=None, n_flight=
     --------
     >>> throughput(size=200, depth=10, density=800)
     """
+
+    # Test input type
+    if not isinstance(size, int):
+        if not isinstance(size, float):
+            raise TypeError("'size' should be either integer or float")
+    if not isinstance(depth, int):
+        if not isinstance(depth, float):
+            raise TypeError("'depth' should be either integer or float")
+    if not isinstance(density, int):
+        if not isinstance(density, float):
+            raise TypeError("'density' should be either integer or float")
+
     # Assign default values
     if pitch == None:
         pitch = size
     if w_flight == None:
         w_flight = size*0.1
-    
+
+    # Tests the types of default variables
+    if not isinstance(rpm, int):
+        if not isinstance(rpm, float):
+            raise TypeError("'rpm' should be either integer or float")
+    if not isinstance(pitch, int):
+        if not isinstance(pitch, float):
+            raise TypeError("'pitch' should be either integer or float")
+    if not isinstance(w_flight, int):
+        if not isinstance(w_flight, float):
+            raise TypeError("'w_flight' should be either integer or float")
+    if not isinstance(n_flight, int):
+        raise TypeError("'n_flight' should be integer")
+
+    # Test input value
+    if depth < size*0.01:
+        raise ValueError("Channel depth is too shallow(<1% of screw size) to be used for extrusion screw")
+    if depth > size*0.3:
+        raise ValueError("Channel depth is too deep(>30% of screw size) to be used for extrusion screw")
+    if size < 5:
+        raise ValueError("Screw size is too small!!")
+    if size > 500:
+        raise ValueError("Screw size is too big!!")
+    if density < 300:
+        raise ValueError("This is not melt density for polymers. Too low!!")
+    if density > 3000:
+        raise ValueError("This is not melt density for polymers. Too high!!")
+    if pitch < size*0.2:
+        raise ValueError("Screw pitch is too small")
+    if pitch > size*2.5:
+        raise ValueError("Screw pitch is too big")
+    if w_flight < size*0.01:
+        raise ValueError("Flight width is too small")
+    if w_flight > size*0.7:
+        raise ValueError("Flight width is too big")
+    if n_flight not in [1, 2]:
+        raise ValueError("You chose wrong value for n_flight. It should be either 1 or 2")
+
     # Calculate basic variables
     screw_root_size = size - (depth*2)
     helix_angle_b = np.arctan(pitch/(np.pi*size))
@@ -77,35 +126,35 @@ def throughput_table(
     Generate a table containing the volumetric throughput with respect to 
     channel depth and screw RPM
     ----------
-    size        : float
+    size        : int or float
                   screw size [mm]
-    density     : float
+    density     : int or float
                   melt density of polymeric material [kg/m^3]
-    pitch       : float
+    pitch       : int or float
                   Screw pitch [mm]
                   If None, squared pitch (=1D) is used
-    w_flight    : float
+    w_flight    : int or float
                   Flight width [mm]
                   If None, 10% of screw size is used for flight width
     n_flight    : int
                   Number of flight [ea]
                   Default value is 1 (single-flighted)
-    min_depth   : float
+    min_depth   : int or float
                   minimum depth for calculation [mm]
                   If None, 2% of screw size is used for minimum depth
-    max_depth   : float
+    max_depth   : int or float
                   maximum depth for calculation [mm]
                   If None, 9% of screw size is used for maximum depth
-    delta_depth : float
+    delta_depth : int or float
                   the amount of increment in depth for calculation [mm]
                   If None, 1% of screw size is used for depth increment
-    min_rpm     : float
+    min_rpm     : int or float
                   minimum screw RPM for calculation [RPM]
                   Default value is 5 (5RPM)
-    max_rpm     : float
+    max_rpm     : int or float
                   maximum screw RPM for calculation [RPM]
                   Default value is 50 (50RPM)
-    delta_rpm   : float
+    delta_rpm   : int or float
                   the amount of increment in RPM for calculation [RPM]
                   Default value is 5 (5RPM)
 
@@ -118,6 +167,15 @@ def throughput_table(
     --------
     >>> output_table(size=200, density=800)
     """
+
+    # Test input type
+    if not isinstance(size, int):
+        if not isinstance(size, float):
+            raise TypeError("'size' should be either integer or float")
+    if not isinstance(density, int):
+        if not isinstance(density, float):
+            raise TypeError("'density' should be either integer or float")
+
     # Assign default values
     if pitch == None:
         pitch = size
@@ -129,7 +187,61 @@ def throughput_table(
         max_depth = size * 0.09
     if delta_depth == None:
         delta_depth = size * 0.01
-    
+
+    # Tests the types of default variables
+    if not isinstance(pitch, int):
+        if not isinstance(pitch, float):
+            raise TypeError("'pitch' should be either integer or float")
+    if not isinstance(w_flight, int):
+        if not isinstance(w_flight, float):
+            raise TypeError("'w_flight' should be either integer or float")
+    if not isinstance(n_flight, int):
+        raise TypeError("'n_flight' should be integer")
+    if not isinstance(min_depth, int):
+        if not isinstance(min_depth, float):
+            raise TypeError("'min_depth' should be either integer or float")
+    if not isinstance(max_depth, int):
+        if not isinstance(max_depth, float):
+            raise TypeError("'max_depth' should be either integer or float")
+    if not isinstance(delta_depth, int):
+        if not isinstance(delta_depth, float):
+            raise TypeError("'delta_depth' should be either integer or float")
+    if not isinstance(min_rpm, int):
+        if not isinstance(min_rpm, float):
+            raise TypeError("'min_rpm' should be either integer or float")
+    if not isinstance(max_rpm, int):
+        if not isinstance(max_rpm, float):
+            raise TypeError("'max_rpm' should be either integer or float")
+    if not isinstance(delta_rpm, int):
+        if not isinstance(delta_rpm, float):
+            raise TypeError("'delta_rpm' should be either integer or float")
+
+        # Test input value
+    if min_depth < size*0.01:
+        raise ValueError("Channel depth is too shallow(<1% of screw size) to be used for extrusion screw")
+    if max_depth > size*0.3:
+        raise ValueError("Channel depth is too deep(>30% of screw size) to be used for extrusion screw")
+    if delta_depth > max_depth-min_depth:
+        raise ValueError("'delta_depth' can not be greater than 'max_depth - min_depth'")
+    if size < 5:
+        raise ValueError("Screw size is too small!!")
+    if size > 500:
+        raise ValueError("Screw size is too big!!")
+    if density < 300:
+        raise ValueError("This is not melt density for polymers. Too low!!")
+    if density > 3000:
+        raise ValueError("This is not melt density for polymers. Too high!!")
+    if pitch < size*0.2:
+        raise ValueError("Screw pitch is too small")
+    if pitch > size*2.5:
+        raise ValueError("Screw pitch is too big")
+    if w_flight < size*0.01:
+        raise ValueError("Flight width is too small")
+    if w_flight > size*0.7:
+        raise ValueError("Flight width is too big")
+    if n_flight not in [1, 2]:
+        raise ValueError("You chose wrong value for n_flight. It should be either 1 or 2")
+
     # Generates table
     table = dict()
     throughput_list = list()
@@ -156,35 +268,35 @@ def throughput_plot(
     Generates a plot containing the volumetric throughput with respect to 
     channel depth and screw RPM
     ----------
-    size        : float
+    size        : int or float
                   screw size [mm]
-    density     : float
+    density     : int or float
                   melt density of polymeric material [kg/m^3]
-    pitch       : float
+    pitch       : int or float
                   Screw pitch [mm]
                   If None, squared pitch (=1D) is used
-    w_flight    : float
+    w_flight    : int or float
                   Flight width [mm]
                   If None, 10% of screw size is used for flight width
     n_flight    : int
                   Number of flight [ea]
                   Default value is 1 (single-flighted)
-    min_depth   : float
+    min_depth   : int or float
                   minimum depth for calculation [mm]
                   If None, 2% of screw size is used for minimum depth
-    max_depth   : float
+    max_depth   : int or float
                   maximum depth for calculation [mm]
                   If None, 9% of screw size is used for maximum depth
-    delta_depth : float
+    delta_depth : int or float
                   the amount of increment in depth for calculation [mm]
                   If None, 1% of screw size is used for depth increment
-    min_rpm     : float
+    min_rpm     : int or float
                   minimum screw RPM for calculation [RPM]
                   Default value is 0 (0RPM)
-    max_rpm     : float
+    max_rpm     : int or float
                   maximum screw RPM for calculation [RPM]
                   Default value is 50 (50RPM)
-    delta_rpm   : float
+    delta_rpm   : int or float
                   the amount of increment in RPM for calculation [RPM]
                   Default value is 1 (1RPM)
 
@@ -197,6 +309,15 @@ def throughput_plot(
     --------
     >>> output_plot(size=200, density=800)
     """
+
+    # Test input type
+    if not isinstance(size, int):
+        if not isinstance(size, float):
+            raise TypeError("'size' should be either integer or float")
+    if not isinstance(density, int):
+        if not isinstance(density, float):
+            raise TypeError("'density' should be either integer or float")
+
     # Assign default values
     if pitch == None:
         pitch = size
@@ -208,7 +329,61 @@ def throughput_plot(
         max_depth = size * 0.09
     if delta_depth == None:
         delta_depth = size * 0.01
-    
+
+    # Tests the types of default variables
+    if not isinstance(pitch, int):
+        if not isinstance(pitch, float):
+            raise TypeError("'pitch' should be either integer or float")
+    if not isinstance(w_flight, int):
+        if not isinstance(w_flight, float):
+            raise TypeError("'w_flight' should be either integer or float")
+    if not isinstance(n_flight, int):
+        raise TypeError("'n_flight' should be integer")
+    if not isinstance(min_depth, int):
+        if not isinstance(min_depth, float):
+            raise TypeError("'min_depth' should be either integer or float")
+    if not isinstance(max_depth, int):
+        if not isinstance(max_depth, float):
+            raise TypeError("'max_depth' should be either integer or float")
+    if not isinstance(delta_depth, int):
+        if not isinstance(delta_depth, float):
+            raise TypeError("'delta_depth' should be either integer or float")
+    if not isinstance(min_rpm, int):
+        if not isinstance(min_rpm, float):
+            raise TypeError("'min_rpm' should be either integer or float")
+    if not isinstance(max_rpm, int):
+        if not isinstance(max_rpm, float):
+            raise TypeError("'max_rpm' should be either integer or float")
+    if not isinstance(delta_rpm, int):
+        if not isinstance(delta_rpm, float):
+            raise TypeError("'delta_rpm' should be either integer or float")
+
+    # Test input value
+    if min_depth < size*0.01:
+        raise ValueError("Channel depth is too shallow(<1% of screw size) to be used for extrusion screw")
+    if max_depth > size*0.3:
+        raise ValueError("Channel depth is too deep(>30% of screw size) to be used for extrusion screw")
+    if delta_depth > max_depth-min_depth:
+        raise ValueError("'delta_depth' can not be greater than 'max_depth - min_depth'")
+    if size < 5:
+        raise ValueError("Screw size is too small!!")
+    if size > 500:
+        raise ValueError("Screw size is too big!!")
+    if density < 300:
+        raise ValueError("This is not melt density for polymers. Too low!!")
+    if density > 3000:
+        raise ValueError("This is not melt density for polymers. Too high!!")
+    if pitch < size*0.2:
+        raise ValueError("Screw pitch is too small")
+    if pitch > size*2.5:
+        raise ValueError("Screw pitch is too big")
+    if w_flight < size*0.01:
+        raise ValueError("Flight width is too small")
+    if w_flight > size*0.7:
+        raise ValueError("Flight width is too big")
+    if n_flight not in [1, 2]:
+        raise ValueError("You chose wrong value for n_flight. It should be either 1 or 2")
+
     # Generate table for plot
     table = dict()
     throughput_list = list()
